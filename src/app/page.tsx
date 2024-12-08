1cebcf19bfff2dee63d15b67712b0c2048cc8ae9
geekskai <geeks.kai@gmail.com>
2024-12-08 15:04:05 +0800

    Refactor homepage layout to include a header and navigation links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,28 @@
-import Link from "next/link";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Link href="/google-doodle-games">google-doodle-games</Link>
+    <div className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <header className="flex flex-wrap sm:justify-start sm:flex-nowrap w-full bg-white text-sm py-3 dark:bg-neutral-800">
+        <nav className="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between">
+          <a
+            className="flex-none font-semibold text-xl text-black focus:outline-none focus:opacity-80 dark:text-white"
+            href="#"
+            aria-label="Brand"
+          >
+            Brand
+          </a>
+          <div className="flex flex-row items-center gap-5 mt-5 sm:justify-end sm:mt-0 sm:ps-5">
+            <a
+              className="font-medium text-blue-500 focus:outline-none"
+              href="/google-doodle-games"
+              aria-current="page"
+            >
+              doodle games
+            </a>
+          </div>
+        </nav>
+      </header>
+      <main className="flex flex-col  justify-center w-full h-full">
+        <p className=" text-center">waiting for content</p>
       </main>
     </div>
   );
